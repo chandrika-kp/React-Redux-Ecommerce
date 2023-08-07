36a69d5ddcbd861c927a8b904878923a5062b1de
unknown <chandrika.450@gmail.com>
2023-08-07 21:37:50 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Navbar from './component/Navbar';
+import Home from './component/Home';
+import Products from './component/Products';
+import Contact from './component/Contact';
+import EachProduct from './component/EachProduct';
+import CartBtn from './component/buttons/CartBtn';
+import About from './component/About'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" Component={Home}/>
+          <Route exact path="/about" Component={About}/>
+          <Route exact path="/products" Component={Products}/>
+          <Route exact path="/products/:id" Component={EachProduct}/>
+          <Route exact path="/contact" Component={Contact}/>
+          <Route exact path="/cart" Component={CartBtn} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
 }
-
 export default App;
